f1ce926132262e89cafd55fa738e4229b171aa4b
Sam Rayatnia <h.rayatnia@gmail.com>
2025-02-03 16:34:20 +0100

    Develop (#3)
    * fix issues with CI/CD (#2)
    
    Update CI/CD issues with coverage
    
    * Update swift.yml

--- Sources/SNetwork/Data+Requestable.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension Data: Requestable {
-    public func set(_ request: Request) -> Request {
-        request.httpBody = self
-        return request
-    }
-}
--- Tests/SNetworkTests/RequestBuilderTests.swift
+++
@@ -1,9 +0,0 @@
-import Testing
-@testable import SNetwork
-
-@Suite("Builder Layer")
-struct RequestBuilderTests {
-    @Test func sampleTest() async throws {
-        #expect(true)
-    }
-}
