2892c5bbe7c67cefea9ec145fb275dbb5961b3d2
Sam Rayatnia <h.rayatnia@gmail.com>
2025-02-03 16:13:19 +0100

    update docs and versioning

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 echo "📖 Generating documentation..."
@@ -7,4 +6,10 @@ swift package --allow-writing-to-directory ./docs \
     generate-documentation --target SNetwork --output-path ./docs \
     --transform-for-static-hosting --hosting-base-path SNetwork
 
-echo "✅ Documentation generated."
+echo "🚀 Deploying documentation to gh-pages branch..."
+git fetch origin gh-pages || git checkout --orphan gh-pages
+git rm -rf .
+mv docs/* .
+touch .nojekyll  # Ensures proper rendering on GitHub Pages
+
+echo "✅ Documentation ready for deployment"
